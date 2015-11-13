cbd70dbea22115b0471a912ac919e7767a49f896
Low WeiLin <lwl1991@gmail.com>
2015-11-13 11:26:51 +0800

    Dir enum

---
+++ Assets/Scripts/Dir.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Dir {Up, Right, Down, Left}
+
+public static class Extensions
+{
+	public static Vec2i vec(this Dir dir)
+	{
+		switch (dir) {
+		case Dir.Up:
+			return new Vec2i(0,1);
+		case Dir.Right:
+			return new Vec2i(1,0);
+		case Dir.Down:
+			return new Vec2i(0,-1);
+		case Dir.Left:
+			return new Vec2i(-1,0);
+		default:
+			return new Vec2i(0,0);
+		}
+	}
+}
