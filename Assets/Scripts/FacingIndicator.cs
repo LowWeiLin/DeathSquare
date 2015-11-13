f5f1dfba74f3965ff7768b87b87a8dc09d85993e
Low WeiLin <lwl1991@gmail.com>
2015-11-13 14:21:06 +0800

    Add health, healthbar and facing indicator

---
+++ Assets/Scripts/FacingIndicator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacingIndicator : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	SpriteRenderer spriteRenderer;
+	EntityBase entity;
+
+
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		entity = GetComponentInParent<EntityBase> ();
+		Debug.Assert (spriteRenderer != null);
+		Debug.Assert (entity != null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spriteRenderer.sprite = sprites [(int)entity.facing];
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	
+	public int maxHp = 10;
+	public int hp = 10;
+	
+	// Use this for initialization
+	void Start () {
+		hp = maxHp;
+	}
+	
+	public void TakeDamage(int damage)
+	{
+		hp -= damage;
+		hp = Mathf.Max (hp, 0);
+		if (hp <= 0)
+		{
+			Debug.Log("Died:" + gameObject);
+			Destroy(this.gameObject);
+		}
+	}
+	
+	public void Heal(int amount)
+	{
+		hp += amount;
+		hp = Mathf.Min (hp, maxHp);
+	}
+	
+}
