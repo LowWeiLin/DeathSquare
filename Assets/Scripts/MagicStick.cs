8eceeda7b3b2664c6356355ab8a3ec7362b881c2
Darius Foo <darius.foo.tw@gmail.com>
2015-11-14 12:07:56 +0800

    Weapon cooldowns

--- Assets/Scripts/MagicStick.cs
+++ Assets/Scripts/MagicStick.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MagicStick : Weapon {
 
-	public override void Attack(PlayerBase player) {
+	public override void PerformAttack(PlayerBase player) {
 		GameObject shot = Instantiate(player.projectile) as GameObject;
 		shot.GetComponent<Projectile>().Init(player.position, player.facing);
 	}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,5 +3,22 @@ using System.Collections;
 
 public abstract class Weapon : MonoBehaviour {
 
-	public abstract void Attack(PlayerBase player);
+	public float cooldown;
+
+	bool enabled = true;
+
+	public void Attack(PlayerBase player) {
+		if (enabled) {
+			PerformAttack(player);
+			StartCoroutine(PutOnCooldown ());
+		}
+	}
+
+	IEnumerator PutOnCooldown() {
+		enabled = false;
+		yield return new WaitForSeconds(cooldown);
+		enabled = true;
+	}
+
+	public abstract void PerformAttack(PlayerBase player);
 }
