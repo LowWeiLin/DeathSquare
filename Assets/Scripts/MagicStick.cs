ca6c75ca93990268ec629546845b4b67914d33fd
Darius Foo <darius.foo.tw@gmail.com>
2015-11-14 13:30:35 +0800

    Polymorphic cooldown field

--- Assets/Scripts/MagicStick.cs
+++ Assets/Scripts/MagicStick.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class MagicStick : Weapon {
 
+	public override float cooldown {
+		get { return 1; }
+		protected set {}
+	}
+
 	public override void PerformAttack(PlayerBase player) {
 		GameObject shot = Instantiate(player.projectile) as GameObject;
 		shot.GetComponent<Projectile>().Init(player.position, player.facing);
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public abstract class Weapon : MonoBehaviour {
 
-	public float cooldown;
+	public abstract float cooldown { get; protected set; }
 
 	bool isUsable = true;
 
