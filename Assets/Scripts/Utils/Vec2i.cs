1bbce873ce24f596f0bf1a8ea7366f4bb17c14a1
Low WeiLin <lwl1991@gmail.com>
2015-11-17 13:59:25 +0800

    Vec2i equals operators

--- Assets/Scripts/Utils/Vec2i.cs
+++ Assets/Scripts/Utils/Vec2i.cs
@@ -13,6 +13,15 @@ public struct Vec2i {
 	public override int GetHashCode() {
 		return x * 10000 + y;	
 	}
+
+	public override bool Equals (object v)
+	{
+		if (! (v is Vec2i)) {
+			return false;
+		} else {
+			return (Vec2i)v == this;
+		}
+	}
 	
 	public bool IsAdjacent(Vec2i v) {
 		if (Mathf.Abs(v.x - x) <= 1 || Mathf.Abs(v.y - y) <= 1) {
@@ -27,6 +36,14 @@ public struct Vec2i {
 	
 	public static Vec2i operator -(Vec2i left, Vec2i right) {
 		return new Vec2i(right.x - left.x, right.y - left.y);
+	}
+
+	public static bool operator ==(Vec2i left, Vec2i right) {
+		return left.x == right.x && left.y == right.y;
+	}
+
+	public static bool operator !=(Vec2i left, Vec2i right) {
+		return !(left == right);
 	}
 
 	public override string ToString () {
