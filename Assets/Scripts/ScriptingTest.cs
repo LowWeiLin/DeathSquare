3e7a5a486cdbabe5187f91a451f82e16d702f52f
Darius Foo <darius.foo.tw@gmail.com>
2015-12-21 09:31:56 +0800

    Scripting basics

---
+++ Assets/Scripts/ScriptingTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniLua;
+
+public class ScriptingTest : MonoBehaviour {
+
+	ILuaState Lua;
+
+	void Start () {
+		Lua = LuaAPI.NewState();
+		Lua.L_OpenLibs();
+		Lua.L_DoString("function fact (n) if n == 0 then return 1 else return n * fact(n-1) end end print(fact(5))");
+	}
+}
