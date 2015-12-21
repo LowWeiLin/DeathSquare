8e08f6063f64b712114dd2a8b91a631169ec3839
Darius Foo <darius.foo.tw@gmail.com>
2015-12-21 16:07:47 +0800

    AI skeleton

---
+++ Assets/Scripts/AIMinion.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIMinion : MonoBehaviour {
+
+	Maybe<GameObject> GetClosestEnemy() {
+		List<GameObject> enemies = new List<GameObject>();
+		// TODO get list of all units which are not on this minion's team
+		if (enemies.Count == 0) {
+			return Maybe<GameObject>.Empty;
+		}
+		// TODO get the closest
+		return enemies[0];
+	}
+
+	bool InAttackRange(GameObject enemy) {
+		float attackRange = 10f;
+		// TODO check if in range
+		return false;
+	}
+
+	void AttackEnemy(GameObject enemy) {
+		// TODO
+	}
+
+	void MoveTowards(GameObject enemy) {
+		// TODO
+	}
+	
+	void Update () {
+		GetClosestEnemy().IfPresent(enemy => {
+			if (InAttackRange(enemy)) {
+				AttackEnemy(enemy);
+			} else {
+				MoveTowards(enemy);
+			}
+		});
+	}
+}
---
+++ Assets/Scripts/Maybe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public struct Maybe<T> {
+	private readonly bool hasValue;
+	public bool HasValue { get { return hasValue; } }
+
+	private readonly T value;
+	public T Value {
+		get {
+			if (!hasValue) {
+				throw new InvalidOperationException();
+			}
+			return value;
+		}
+	}
+		
+	public static Maybe<T> Empty {
+		get {
+			return new Maybe<T>(false, default(T));
+		}
+	}
+
+	public static Maybe<T> Of(T value) {
+		return new Maybe<T>(true, value);
+	}
+		
+	private Maybe(bool hasValue, T value) {
+		this.hasValue = hasValue;
+		this.value = value;
+	}
+
+	public delegate void F(T value);
+
+	public void IfPresent(F f) {
+		if (hasValue) {
+			f(value);
+		}
+	}
+
+	public static implicit operator Maybe<T>(T value) {
+		return new Maybe<T>(true, value);
+	}
+}
