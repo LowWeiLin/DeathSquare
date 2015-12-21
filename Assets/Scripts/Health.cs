ff7deec753c7f0254bbadd24416e0dfec16f1017
Low WeiLin <lwl1991@gmail.com>
2015-12-21 15:55:25 +0800

    Add health, health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	
+	EntityBase entity;
+	
+	public int maxHp = 10;
+	public int hp = 10;
+	
+	// Use this for initialization
+	void Start () {
+		entity = GetComponent<EntityBase> ();
+		hp = maxHp;
+	}
+	
+	public void TakeDamage(int damage)
+	{
+		hp -= damage;
+		hp = Mathf.Max (hp, 0);
+		if (hp <= 0)
+		{
+			entity.DestroyEntity();
+		}
+	}
+	
+	public void Heal(int amount)
+	{
+		hp += amount;
+		hp = Mathf.Min (hp, maxHp);
+	}
+	
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Health))]
+public class HealthBar : MonoBehaviour
+{
+	public Rect m_Rectangle = new Rect(0, 0, 40, 4);
+	public Vector2 m_Offset = new Vector2(-20, -40);
+	
+	Texture2D m_Background;//Red background
+	Texture2D m_Foreground;//Green bar foreground
+	
+	Health m_Health;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		// Use 1x1 pixels to draw the health bars
+		m_Background = new Texture2D(1,1);
+		m_Background.SetPixel(0,0, Color.red);
+		m_Background.Apply();
+		m_Foreground = new Texture2D(1,1);
+		m_Foreground.SetPixel(0,0, Color.green);
+		m_Foreground.Apply();
+		
+		m_Health = GetComponent<Health>();
+	}
+	
+	void OnGUI ()
+	{
+		//Determine positions and draw bars
+		//Red background
+		m_Rectangle.x = Camera.main.WorldToScreenPoint(transform.position).x + m_Offset.x;
+		m_Rectangle.y = Screen.height - Camera.main.WorldToScreenPoint(transform.position).y + m_Offset.y;
+		
+		GUI.DrawTexture(m_Rectangle, m_Background);
+		
+		//Green bar foreground
+		Rect partialRect = m_Rectangle;
+		partialRect.width = m_Rectangle.width * ((float)m_Health.hp/(float)m_Health.maxHp);
+		partialRect.x = m_Rectangle.x;
+		
+		GUI.DrawTexture(partialRect, m_Foreground);
+	}
+}
