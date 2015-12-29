c9dace376897ea03da61e20ea93eeb0c17eb21cf
Low WeiLin <lwl1991@gmail.com>
2015-12-29 11:45:29 +0800

    Fix camera follow

--- Assets/Scripts/Visual/CameraFollow.cs
+++ Assets/Scripts/Visual/CameraFollow.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour 
-{
-	public float xMargin = 1f;		// Distance in the x axis the player can move before the camera follows.
-	public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
-	public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
-	public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
-	public Vector2 maxXAndY = new Vector2(50, 50);		// The maximum x and y coordinates the camera can have.
-	public Vector2 minXAndY = new Vector2(-50, -50);		// The minimum x and y coordinates the camera can have.
-	public bool disabled = true;
+public class CameraFollow : MonoBehaviour {
 
-	private Transform player;		// Reference to the player's transform.
+	
+	GameObject target;
+
+	float damping = 0;
+	bool disabled = true;
+
+	float cameraDist = 5f;
+	Vector3 offsetBase = new Vector3(Mathf.Sqrt (9f/2f),2f,Mathf.Sqrt (9f/2f));
+	Vector3 offset;
 
 	void Start() {
+		offsetBase.Normalize ();
+		offset = offsetBase * cameraDist;
 		StartCoroutine(LookForPlayer());
 	}
-
+	
 	IEnumerator LookForPlayer() {
 		GameObject playerObj = null;
 		while (disabled) {
@@ -30,56 +32,15 @@ public class CameraFollow : MonoBehaviour
 		Util.Assert(playerObj != null);
 		Init(playerObj);
 	}
-
+	
 	public void Init(GameObject playerObj) {
-		player = playerObj.transform;
+		target = playerObj;
 		disabled = false;
 	}
 
-	bool CheckXMargin()
-	{
-		// Returns true if the distance between the camera and the player in the x axis is greater than the x margin.
-		return Mathf.Abs(transform.position.x - player.position.x) > xMargin;
-	}
+	void Update() {
 
-
-	bool CheckYMargin()
-	{
-		// Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
-		return Mathf.Abs(transform.position.z - player.position.z) > yMargin;
-	}
-
-	void LateUpdate ()
-	{
-		if (!disabled) {
-			TrackPlayer();
-		}
-	}
-
-
-	void TrackPlayer ()
-	{
-		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
-		float targetX = transform.position.x;
-		float targetY = transform.position.z;
-
-		// If the player has moved beyond the x margin...
-		if(CheckXMargin()) {
-			// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
-			targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.deltaTime);
-		}
-
-		// If the player has moved beyond the y margin...
-		if(CheckYMargin()) {
-			// ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
-			targetY = Mathf.Lerp(transform.position.z, player.position.z, ySmooth * Time.deltaTime);
-		}
-
-		// The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
-		targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
-		targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
-
-		// Set the camera's position to the target position with the same z component.
-		transform.position = new Vector3(targetX, transform.position.z, targetY);
+		transform.position = target.transform.position + offset;
+		transform.LookAt(target.transform);
 	}
 }
